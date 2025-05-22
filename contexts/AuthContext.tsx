7305c1db33e1beb8ffc666c547b18efabb3c2a02
Mariano Rodriguez <yomariano05@gmail.com>
2025-05-22 11:03:42 +0100

    fix

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -167,7 +167,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: window.location.origin,
+          redirectTo: 'https://bookzify.xyz/auth/callback', // Change this line
           queryParams: {
             access_type: 'offline',
             prompt: 'consent',
